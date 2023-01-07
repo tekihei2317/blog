a284f88c25e2a393254c918e9cea9795f0b6775c
tekihei2317 <tekihei2317@gmail.com>
2023-01-07 09:24:43 +0900

    feat: PVを計測できるようにGoogle Analyticsを設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics, usePageView } from '../utils/google-analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  usePageView();
+
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />;
+    </>
+  );
 }
