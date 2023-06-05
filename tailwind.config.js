93f1fb162294dda858eeaf6a768e156f608c5dbf
tekihei2317 <tekihei2317@gmail.com>
2023-06-05 20:28:36 +0900

    tailwind.config.jsからpagesディレクトリを削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{ts,tsx}', './pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
